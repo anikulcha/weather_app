043e5545bcf04c759cc75b0d3ab199ba8716ae4e
andrey <nikulcha1998@gmail.com>
2023-02-05 00:03:51 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // api.weahterstack.com/current?access_key=e126cf22729c9dac12af8afeef12e463
 const link = 'http://api.weatherstack.com/current?access_key=bacd6314b108e58f252208915e4cf55e';
 
+const store = {
+    city: 'London',
+}
+
 const fetchData = async () => {
-    const result = await fetch(`${link}&query=London`);
+    const result = await fetch(`${link}&query=${store.city}`);
     const data = await result.json();
 
     console.log(data)
